94c8639905fda7c987ab40cfd6290e7a2ee5369e
cmd <123102218+cmdscripts@users.noreply.github.com>
2023-07-28 15:55:59 +0200

    Update server.lua

--- source/server.lua
+++ source/server.lua
@@ -3,7 +3,7 @@ AddEventHandler('cmdSearchTrash:trash', function(item, count)
   local xPlayer = ESX.GetPlayerFromId(source)
 
   xPlayer.addInventoryItem(item, count)
-  MSK.Notification(source, 'Trash', 'You received ' ..count.. ' ' ..ESX.GetItemLabel(item).. '',5000)
+  MSK.Notification(source, 'Trash', 'You received ' ..count.. ' ' ..ESX.GetItemLabel(item).. 'info' ,5000)
 
   -- Send Discord webhook
 
