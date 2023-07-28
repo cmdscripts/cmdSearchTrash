7abb3d59baeeaf9bf4012f891ad0be4553b3b9de
cmd <123102218+cmdscripts@users.noreply.github.com>
2023-07-28 16:32:43 +0200

    Update server.lua

--- source/server.lua
+++ source/server.lua
@@ -5,19 +5,4 @@ AddEventHandler('cmdSearchTrash:trash', function(item, count)
   xPlayer.addInventoryItem(item, count)
   MSK.Notification(source, 'Trash', 'You received ' ..count.. ' ' ..ESX.GetItemLabel(item), 'info', 5000)
 
-  -- Send Discord webhook
-
-end)
-AddEventHandler('onResourceStart', function()
-  if GetCurrentResourceName() == 'cmdSearchTrash' then
-      print('########################')
-      print('Script loaded. All fine!')
-      print('########################')
-  else
-    print('You are not allowed to rename this script, server is stopping!')
-    print('You are not allowed to rename this script, server is stopping!')
-    print('You are not allowed to rename this script, server is stopping!')
-    Wait(3000)
-    os.exit()
-  end
 end)
