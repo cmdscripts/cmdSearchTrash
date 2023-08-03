323cb5de530c69f62b5498a6407e3568e312459e
cmd <123102218+cmdscripts@users.noreply.github.com>
2023-08-03 14:08:36 +0200

    Update server.lua

--- source/server.lua
+++ source/server.lua
@@ -1,8 +1,13 @@
 RegisterNetEvent('cmdSearchTrash:trash')
 AddEventHandler('cmdSearchTrash:trash', function(item, count)
   local xPlayer = ESX.GetPlayerFromId(source)
-
-  xPlayer.addInventoryItem(item, count)
-  MSK.Notification(source, 'Trash', 'You received ' ..count.. ' ' ..ESX.GetItemLabel(item), 'info', 5000)
-
+    
+   if xPlayer.canCarryItem(item, count) then
+    xPlayer.addInventoryItem(item, count)
+    MSK.Notification(source, 'Trash', 'You received ' ..count.. ' ' ..ESX.GetItemLabel(item), 'info', 5000)
+   else
+    --xPlayer.showNotification("Deine Taschen sind zu voll")
+      MSK.Notification(source, 'Trash', 'Deine Taschen sind zu voll', 'info', 5000)
+   end
 end)
+
